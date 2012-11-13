b2d35d0e5a042fd4bfb66ed749b70dcb9302da7a
Kunal Bhatt <bhattk09@gmail.com>
2012-11-13 00:55:21 -0800

    changed application html

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,17 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require_tree .
+
+//set path
+ZeroClipboard.setMoviePath('ZeroClipboard.swf');
+//create client
+var clip = new ZeroClipboard.Client();
+//event
+clip.addEventListener('mousedown',function() {
+  clip.setText(document.getElementById('box-content').value);
+});
+clip.addEventListener('complete',function(client,text) {
+  alert('copied: ' + text);
+});
+//glue it to the button
+clip.glue('copy'); 
