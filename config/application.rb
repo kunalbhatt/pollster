b8db1a117834e17f5fab26f7db880fa0cb17f897
Kunal Bhatt <bhattk09@gmail.com>
2012-11-14 22:29:03 -0800

    added config asset to applications.rb

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
+
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
@@ -11,6 +13,7 @@ end
 
 module Pollster
   class Application < Rails::Application
+     config.assets.initialize_on_precompile=false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -58,5 +61,7 @@ module Pollster
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+
   end
 end
